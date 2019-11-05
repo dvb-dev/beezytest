8c688fc53b00bce318580714a439b455e64ac9de
dvb <dvb.dev@protonmail.com>
2019-11-05 07:06:18 +0100

    Add response objects for all endpoints.

---
+++ BeezyTest.Api/BeezyTest.Api/Dto/Result/Cinema/Billboard/SimpleBillbaordResult.cs
@@ -0,0 +1,13 @@
+﻿using BeezyTest.DomainEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeezyTest.Api.Dto.Result.Cinema.Billboard
+{
+	public class SimpleBillbaordResult
+	{
+		public SimpleBillboard Billboard { get; set; }
+	}
+}
---
+++ BeezyTest.Api/BeezyTest.Api/Dto/Result/Viewer/MovieRecomendationList.cs
@@ -0,0 +1,13 @@
+﻿using BeezyTest.DomainEntities.Media;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeezyTest.Api.Dto.Result.Viewer
+{
+	public class MovieRecomendationList
+	{
+		public IList<Movie> movies;
+	}
+}
